f8b324f8680e7a7f74f0d738d33d51ccd27a2742
kolo <sabatt148@gmail.com>
2024-07-04 21:16:46 +0300

    some new

--- events/for_all_events/me_file.py
+++ events/for_all_events/me_file.py
@@ -1,11 +1,25 @@
-async def me(event, client):
+import os
 
+
+async def me(event, client):
     chat = await event.get_chat()
     sender = await event.get_sender()
 
-    await client.send_message(chat, f'<pre>id = {sender.id}</pre>\n'
-                                    f'<pre>bot = {sender.bot}</pre>\n'
-                                    f'<pre>premium = {sender.premium}</pre>\n'
-                                    f'<pre>first name = {sender.first_name}</pre>\n'
-                                    f'<pre>last name = {sender.last_name}</pre>\n'
-                                    f'<pre>username = {sender.username}</pre>\n', parse_mode='HTML')
+    caption = f'<pre>id = {sender.id}</pre>\n' \
+              f'<pre>bot = {sender.bot}</pre>\n' \
+              f'<pre>premium = {sender.premium}</pre>\n' \
+              f'<pre>first name = {sender.first_name}</pre>\n' \
+              f'<pre>last name = {sender.last_name}</pre>\n' \
+              f'<pre>username = {sender.username}</pre>\n'
+
+    path = 'stickers_data/my_ava.png'
+    avatar = await client.download_profile_photo(sender.id, file=path)
+
+    if not avatar:
+
+        await client.send_message(chat, caption, parse_mode='HTML')
+
+    else:
+        await client.send_file(chat, avatar, caption=caption, parse_mode='HTML')
+
+    os.remove(path)
--- events/only_my_events/you_file.py
+++ events/only_my_events/you_file.py
@@ -1,10 +1,24 @@
-async def you(event, client):
+import os
 
+
+async def you(event, client):
     chat = await event.get_chat()
 
-    await client.send_message(chat, f'<pre>id = {chat.id}</pre>\n'
-                                    f'<pre>bot = {chat.bot}</pre>\n'
-                                    f'<pre>premium = {chat.premium}</pre>\n'
-                                    f'<pre>first name = {chat.first_name}</pre>\n'
-                                    f'<pre>last name = {chat.last_name}</pre>\n'
-                                    f'<pre>username = {chat.username}</pre>\n', parse_mode='HTML')
+    caption = f'<pre>id = {chat.id}</pre>\n' \
+              f'<pre>bot = {chat.bot}</pre>\n' \
+              f'<pre>premium = {chat.premium}</pre>\n' \
+              f'<pre>first name = {chat.first_name}</pre>\n' \
+              f'<pre>last name = {chat.last_name}</pre>\n' \
+              f'<pre>username = {chat.username}</pre>\n'
+
+    path = 'stickers_data/your_ava.png'
+    avatar = await client.download_profile_photo(chat.id, file=path)
+
+    if not avatar:
+
+        await client.send_message(chat, caption, parse_mode='HTML')
+
+    else:
+        await client.send_file(chat, avatar, caption=caption, parse_mode='HTML')
+
+    os.remove(path)
