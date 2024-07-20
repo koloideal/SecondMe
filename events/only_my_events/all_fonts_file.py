51fb08ea7c317c997f9600a73ee9e9a658e335e5
koloideal <sabatt148@gmail.com>
2024-07-20 13:48:05 +0300

    some

--- events/only_my_events/all_fonts_file.py
+++ events/only_my_events/all_fonts_file.py
@@ -1,5 +1,6 @@
 import os
 from telethon.events import NewMessage
+from configparser import ConfigParser
 
 
 async def all_fonts(event: NewMessage.Event) -> None:
@@ -8,11 +9,19 @@ async def all_fonts(event: NewMessage.Event) -> None:
 
     all_fonts_files = os.listdir(path)
 
+    config = ConfigParser()
+    config.read('secret_data/config.ini')
+
+    now_font = config['System']['path_to_font']
+
     if all_fonts_files:
 
         all_fonts_files = '\n\n'.join([f'<code>{x}</code>' for x in all_fonts_files])
 
-        await event.reply(all_fonts_files, parse_mode='HTML')
+        await event.reply(f'<pre>Now font:</pre>'
+                          f'{now_font}\n\n'
+                          f'<pre>All Fonts:</pre>'
+                          f'{all_fonts_files}', parse_mode='HTML')
 
     else:
 
